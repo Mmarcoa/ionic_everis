0894ce1d7f633ab8f7506f43f0e1a19d3add0f5a
Marco A. Olvera <molvera@onikom.com.mx>
2017-05-22 17:27:25 -0500

    servicio json

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { PeopleServiceProvider } from '../../providers/people-service/people-service';
+
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  public people: any;
+  constructor(public peopleService: PeopleServiceProvider, public navCtrl: NavController) {
+      this.getIceCream();
+  }
 
-  constructor(public navCtrl: NavController) {
-
+  getIceCream() {
+    console.log('Helado');
+    this.peopleService.getData().
+    then(data => {
+      this.people = data;
+    });
   }
 
 }
--- src/providers/people-service/people-service.ts
+++ src/providers/people-service/people-service.ts
@@ -10,9 +10,28 @@ import 'rxjs/add/operator/map';
 */
 @Injectable()
 export class PeopleServiceProvider {
+  hadIceCream: boolean = false;
+  data: any;
 
   constructor(public http: Http) {
     console.log('Hello PeopleServiceProvider Provider');
+  }
+
+  getData() {
+    this.hadIceCream = true;
+    return new Promise(resolve => {
+    // We're using Angular HTTP provider to request the data,
+    // then on the response, it'll map the JSON data to a parsed JS object.
+    // Next, we process the data and resolve the promise with the new data.
+    this.http.get('https://randomuser.me/api/?results=10')
+      .map(res => res.json())
+      .subscribe(data => {
+        // we've got back the raw data, now generate the core schedule data
+        // and save the data for later reference
+        this.data = data.results;
+        resolve(this.data);
+      });
+  });
   }
 
 }
